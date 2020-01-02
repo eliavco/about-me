a544b69ccf562f5a4d88cab8ee69685e0ee44974
Eliav <eliav.s.cohen@gmail.com>
2020-01-02 12:07:51 +0200

    2/1/2020 12:07AM

--- public/js/login.js
+++ public/js/login.js
@@ -2,8 +2,6 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
 
-const whereToGo = 'https://eliav.herokuapp.com/go';
-
 export const login = async (email, password) => {
     try {
         const res = await axios({
@@ -20,7 +18,7 @@ export const login = async (email, password) => {
         if (res.data.status === 'success') {
             showAlert('success', 'Logged in successfully!');
             setTimeout(() => {
-                location.assign(whereToGo);
+                location.assign('/go');
             }, 1500);
         }
 
@@ -51,7 +49,7 @@ export const signup = async (name, email, password, passwordConfirm) => {
         if (res.data.status === 'success') {
             showAlert('success', 'Signed up successfully!');
             setTimeout(() => {
-                location.assign(whereToGo);
+                location.assign('/go');
             }, 1500);
         }
 
@@ -75,7 +73,7 @@ export const logout = async () => {
         // IF reload not set with true the browser might reload the page from the cache and not GET the current page again from the server
         if (res.data.status === 'success') {
             if (window.location.pathname in defaultRoutes) return location.reload(true);
-            return location.assign(whereToGo);
+            return location.assign('/go');
         }
     } catch (err) {
         showAlert('error', 'Error while logging out! Please try again later.')
